6792846954d3775414daedbd6048186b1c1ad917
maizadv <021maizadv@gmail.com>
2022-09-22 15:13:31 -0300

    adiciona const perimeter

--- Fundamentos/bloco04_intro_JavaScript/4.1-Primeiros_passos/ex4.1.2.js
+++ Fundamentos/bloco04_intro_JavaScript/4.1-Primeiros_passos/ex4.1.2.js
@@ -1,6 +1,7 @@
 const base = 5;
 const heigth = 8;
-const area = base * heigth
+const area = base * heigth;
+const perimeter = (2 * 5) + ( 2 * 8);
 
 console.log(base, heigth);
-console.log(area);
+console.log(area, perimeter);

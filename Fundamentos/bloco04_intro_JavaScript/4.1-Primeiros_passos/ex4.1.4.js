abb62c475b96cf77203b8b096ca3583fa2668c0f
maizadv <021maizadv@gmail.com>
2022-09-22 17:10:17 -0300

    adiciona condição de estrutura com &&

--- Fundamentos/bloco04_intro_JavaScript/4.1-Primeiros_passos/ex4.1.4.js
+++ Fundamentos/bloco04_intro_JavaScript/4.1-Primeiros_passos/ex4.1.4.js
@@ -1,4 +1,20 @@
 // &&
 
-const currentHour = 20;
+const currentHour = 5;
 let message = "";
+
+if (currentHour >= 22) {
+    console.log(message += "Não deveríamos comer nada, é hora de dormir");
+} 
+else if (currentHour >= 18 && currentHour < 22) {
+    console.log(message += "Rango da noite, vamos jantar :D")
+}
+else if (currentHour >= 14 && currentHour < 18) {
+    console.log(message += "Vamos fazer um bolo pro café da tarde?" );
+}
+else if (currentHour >= 11 && currentHour <= 14) {
+    console.log(message += "Hora do almoço!!!" )
+}
+else if (currentHour >= 4 && currentHour <= 11) {
+    console.log(message += "Hmmm, cheiro de café recém-passado" )
+}

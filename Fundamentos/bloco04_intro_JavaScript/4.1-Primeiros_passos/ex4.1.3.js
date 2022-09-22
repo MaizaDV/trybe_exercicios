8a46ce7d70fa6a9423c142cb57a54ef6335b6614
maizadv <021maizadv@gmail.com>
2022-09-22 16:30:14 -0300

    adiciona estrutura condicional else if para notas

--- Fundamentos/bloco04_intro_JavaScript/4.1-Primeiros_passos/ex4.1.3.js
+++ Fundamentos/bloco04_intro_JavaScript/4.1-Primeiros_passos/ex4.1.3.js
@@ -1,1 +1,11 @@
 const notaPessoa = 50;
+
+if (notaPessoa >= 80) {
+    console.log("Parabéns, você foi aprovado(a)!");
+} 
+else if (notaPessoa >= 60  && notaPessoa < 80) {
+    console.log("Você está na nossa lista de espera");
+}
+else {
+    console.log("Você foi reprovado(a)")
+}

c2941b628fc32cfca12f8228f2c084db2378bac2
maizadv <021maizadv@gmail.com>
2022-09-22 19:22:52 -0300

    adiciona valores para a estrutura switch case

--- Fundamentos/bloco04_intro_JavaScript/4.1-Primeiros_passos/ex4.1.5.js
+++ Fundamentos/bloco04_intro_JavaScript/4.1-Primeiros_passos/ex4.1.5.js
@@ -1,8 +1,17 @@
 let pessoaCandidata;
 
 switch (pessoaCandidata) {
-    case "":
-        console.log("");
+    case "aprovada":
+        console.log("Parabéns, você foi aprovada(o)!");
         break;
-        
+
+    case "lista":
+        console.log("Você está na nossa lista de espera");
+        break;
+
+    case "reprovada":
+        console.log("Você foi reprovada(o)");
+        break;
+    default:
+        console.log("Informação incorreta");
 }

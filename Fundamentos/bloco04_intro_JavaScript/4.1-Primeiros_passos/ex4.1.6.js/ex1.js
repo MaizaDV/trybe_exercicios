73c5e92b2c66917b0a2ac261e1a284bcc83816a0
maizadv <021maizadv@gmail.com>
2022-09-23 03:19:34 -0300

    exercício prático 3

--- Fundamentos/bloco04_intro_JavaScript/4.1-Primeiros_passos/ex4.1.6.js/ex1.js
+++ Fundamentos/bloco04_intro_JavaScript/4.1-Primeiros_passos/ex4.1.6.js/ex1.js
@@ -1,7 +1,7 @@
 // 1
 const a = Math.floor(Math.random() * 10) + 1;
 const b = Math.floor(Math.random() * 10) + 1;
-console.log("Letra a = " + a + " Letra b = " + b );
+console.log("Letra A = " + a + " Letra B = " + b );
 
 console.log("Adição " + (a + b));
 console.log("Subtração " + (a - b));
@@ -13,5 +13,16 @@ console.log("Módulo " + (a % b));
 if (a > b) {
     console.log("A é maior que B.");
 } else {
-    console.log("B é mair que A.");
+    console.log("B é maior que A.");
+}
+
+// 3
+const c = Math.floor(Math.random() * 10) + 1;
+console.log("Letra C = " + c);
+if (a > b && a > c) {
+    console.log("A é maior que B e C");
+} else if (b > a && b > c) {
+    console.log("B é maior que A e c");
+} else {
+    console.log("C é maior que A e B");
 }

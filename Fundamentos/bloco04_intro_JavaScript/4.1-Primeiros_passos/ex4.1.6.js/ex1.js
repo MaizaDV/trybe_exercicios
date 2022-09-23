066408093d14d7bdef518c2b276d2baf530b1587
maizadv <021maizadv@gmail.com>
2022-09-22 23:24:34 -0300

    exercício prático 1

---
+++ Fundamentos/bloco04_intro_JavaScript/4.1-Primeiros_passos/ex4.1.6.js/ex1.js
@@ -0,0 +1,9 @@
+// 1
+const a = Math.floor(Math.random() * 10) + 1;
+const b = Math.floor(Math.random() * 10) + 1;
+
+console.log("Adição " + (a + b));
+console.log("Subtração " + (a - b));
+console.log("Multiplicação " + (a * b));
+console.log("Divisão " + (a / b));
+console.log("Módulo " + (a % b));

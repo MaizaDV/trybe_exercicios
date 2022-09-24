7de449053a6b6a723051da1c90373bb4ada5b417
maizadv <021maizadv@gmail.com>
2022-09-24 02:24:12 -0300

    exercício 4.2.1

---
+++ Fundamentos/bloco04_intro_JavaScript/4.1-Primeiros_passos/ex4.1.6.js/ex11.js
@@ -0,0 +1,31 @@
+
+let aliquotaINSS;
+let aliquotaIR;
+
+const salarioBruto = 3700.00;
+
+if (salarioBruto <= 1556.94) {
+    aliquotaINSS = salarioBruto * 0.08;
+} else if (salarioBruto <= 2594.92) {
+    aliquotaINSS = salarioBruto * 0.09;
+} else if (salarioBruto <= 5189.82) {
+    aliquotaINSS = salarioBruto * 0.11;
+} else {
+    aliquotaINSS = 570.88;
+}
+
+const baseSalary = salarioBruto - aliquotaINSS;
+
+if (salarioBase <= 1903.98) {
+    aliquotaIR = 0;
+} else if (salarioBase <= 2826.65) {
+    aliquotaIR = (salarioBase * 0.075) - 142.80;
+} else if (salarioBase <= 3751.05) {
+    aliquotaIR = (salarioBase * 0.15) - 354.80;
+} else if (salarioBase <= 4664.68) {
+    aliquotaIR = (salarioBase * 0.225) - 636.13;
+} else {
+    aliquotaIR = (salarioBase * 0.275) - 869.36;
+};
+
+console.log("Salário: " + (salarioBase - aliquotaIR));

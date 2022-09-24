080798307bb555d2722f0fb0c46ed696fd3e8ee7
maizadv <021maizadv@gmail.com>
2022-09-24 03:54:41 -0300

    descobre números ímpares

--- Fundamentos/bloco04_intro_JavaScript/4.2-Array e LoopFor/4.2.1.js
+++ Fundamentos/bloco04_intro_JavaScript/4.2-Array e LoopFor/4.2.1.js
@@ -1,17 +1,23 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = contador = maiorValor = 0;
+let soma = contador = maiorValor = numeroImpar = 0;
 
 for (let i =0; i < numbers.length; i += 1) {
     soma += numbers[i];
     contador += 1;
-    if(numbers[i] >= maiorValor) {
+    if (numbers[i] % 2 !== 0) {
+        numeroImpar += 1;
+        if (numeroImpar === 0) {
+            console.log("Nenhum valor ímpar encontrado")
+        };
+    } else if (numbers[i] >= maiorValor) {
         maiorValor = numbers[i];
-    }
+    };
 } if (soma/contador > 20) {
     console.log("Valor maior que 20.");
 } else {
     console.log("Valoer menor ou igual a 20");
-}
+};
 console.log("Soma dos valores da array: " + soma);
 console.log("A média aritmética do array: " + soma / contador);
 console.log("Maior valor da array: " + maiorValor);
+console.log("Existem " + numeroImpar + " números ímpares nessa array.");

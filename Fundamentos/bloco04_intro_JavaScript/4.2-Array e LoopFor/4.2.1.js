61c8cc5a3ba15b7cec42e549a282370d41df7eb0
maizadv <021maizadv@gmail.com>
2022-09-24 03:38:06 -0300

    descobre o maior valor da array

--- Fundamentos/bloco04_intro_JavaScript/4.2-Array e LoopFor/4.2.1.js
+++ Fundamentos/bloco04_intro_JavaScript/4.2-Array e LoopFor/4.2.1.js
@@ -1,11 +1,12 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = contador = 0;
+let soma = contador = maiorValor = 0;
 
 for (let i =0; i < numbers.length; i += 1) {
     soma += numbers[i];
     contador += 1;
-    console.log(numbers[i], contador);
-    
+    if(numbers[i] >= maiorValor) {
+        maiorValor = numbers[i];
+    }
 } if (soma/contador > 20) {
     console.log("Valor maior que 20.");
 } else {
@@ -13,3 +14,4 @@ for (let i =0; i < numbers.length; i += 1) {
 }
 console.log("Soma dos valores da array: " + soma);
 console.log("A média aritmética do array: " + soma / contador);
+console.log("Maior valor da array: " + maiorValor);

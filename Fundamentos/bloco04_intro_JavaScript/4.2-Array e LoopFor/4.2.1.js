a5c15ff85a7217e2d3be90f668633193a9a97533
maizadv <021maizadv@gmail.com>
2022-09-24 03:08:54 -0300

    soma dos valores da array

--- Fundamentos/bloco04_intro_JavaScript/4.2-Array e LoopFor/4.2.1.js
+++ Fundamentos/bloco04_intro_JavaScript/4.2-Array e LoopFor/4.2.1.js
@@ -1,5 +1,7 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-
+let soma = 0;
 for (let i =0; i < numbers.length; i += 1) {
+    soma += numbers[i];
     console.log(numbers[i]);
 };
+console.log("Soma dos valores da array: " + soma);

8f9e1fee6c1b1447d00f88ce3da38e3a1fe4cf71
maizadv <021maizadv@gmail.com>
2022-09-24 03:17:47 -0300

    calcula média aritmética do array

--- Fundamentos/bloco04_intro_JavaScript/4.2-Array e LoopFor/4.2.1.js
+++ Fundamentos/bloco04_intro_JavaScript/4.2-Array e LoopFor/4.2.1.js
@@ -1,7 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = 0;
+let soma = contador = 0;
+
 for (let i =0; i < numbers.length; i += 1) {
     soma += numbers[i];
-    console.log(numbers[i]);
+    contador += 1;
+    console.log(numbers[i], contador);
 };
 console.log("Soma dos valores da array: " + soma);
+console.log("A média aritmética do array: " + soma / contador);

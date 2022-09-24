019450086ad4448470a77a92862ea8d9c0294fef
maizadv <021maizadv@gmail.com>
2022-09-24 04:12:42 -0300

    divide por 2 cada elemento da array

--- Fundamentos/bloco04_intro_JavaScript/4.2-Array e LoopFor/4.2.1.js
+++ Fundamentos/bloco04_intro_JavaScript/4.2-Array e LoopFor/4.2.1.js
@@ -33,3 +33,8 @@ for (let i = 1; i <= 25; i += 1) {
     numeros.push(i);
 }
 console.log(numeros);
+
+for (let index = 0; index < numeros.length; index += 1) {
+    console.log(numeros[index] / 2);
+};
+

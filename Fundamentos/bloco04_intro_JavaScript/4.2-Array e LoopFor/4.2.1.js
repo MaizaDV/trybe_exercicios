f6ec26a522c3b5702dde413b6b2c94bbf13e35b5
maizadv <021maizadv@gmail.com>
2022-09-24 03:23:24 -0300

    verifica se o valor final é maior que 20

--- Fundamentos/bloco04_intro_JavaScript/4.2-Array e LoopFor/4.2.1.js
+++ Fundamentos/bloco04_intro_JavaScript/4.2-Array e LoopFor/4.2.1.js
@@ -5,6 +5,11 @@ for (let i =0; i < numbers.length; i += 1) {
     soma += numbers[i];
     contador += 1;
     console.log(numbers[i], contador);
-};
+    
+} if (soma/contador > 20) {
+    console.log("Valor maior que 20.");
+} else {
+    console.log("Valoer menor ou igual a 20");
+}
 console.log("Soma dos valores da array: " + soma);
 console.log("A média aritmética do array: " + soma / contador);

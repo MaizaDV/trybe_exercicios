42db4f1e2194365fd8e86616e6314cec1b72960c
maizadv <021maizadv@gmail.com>
2022-09-24 06:17:22 -0300

    cria nova array a partir de multiplicação da antiga array

--- Fundamentos/bloco04_intro_JavaScript/4.2-Array e LoopFor/4.2.2.js
+++ Fundamentos/bloco04_intro_JavaScript/4.2-Array e LoopFor/4.2.2.js
@@ -18,15 +18,28 @@
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (let index = 1; index < numbers.length; index += 1) {
-    for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
-        if (numbers[index] > numbers[secondIndex]) {
-            let position = numbers[index];
-            numbers[index] = numbers[secondIndex];
-            numbers[secondIndex] = position;
-        }
+// for (let index = 1; index < numbers.length; index += 1) {
+//     for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+//         if (numbers[index] > numbers[secondIndex]) {
+//             let position = numbers[index];
+//             numbers[index] = numbers[secondIndex];
+//             numbers[secondIndex] = position;
+//         }
+//     }
+// }
+
+// console.log(numbers);
+
+let newArray = [];
+
+for (let index = 0; index < numbers.length; index += 1) {
+    if (index + 1 < numbers.length) {
+        newArray.push(numbers[index] * numbers[index + 1]);
+    } else {
+        newArray.push(numbers[index] * 2);
     }
 }
 
-console.log(numbers);
+console.log(newArray);
+
 

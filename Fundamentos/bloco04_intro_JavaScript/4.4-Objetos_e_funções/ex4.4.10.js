cdbdee9e4a68eaf63538f3846cc499578fd38a20
maizadv <021maizadv@gmail.com>
2022-09-28 04:04:06 -0300

    exrcício 9 funções for in maior palavra, Dia 4, seção 4

---
+++ Fundamentos/bloco04_intro_JavaScript/4.4-Objetos_e_funções/ex4.4.10.js
@@ -0,0 +1,11 @@
+function maiorPalavra(palavras) {
+    let maiorPalavra = palavras[0];
+    for (let indice in palavras) {
+      if (maiorPalavra.length < palavras[indice].length) {
+        maiorPalavra = palavras[indice];
+      }
+    }
+    return maiorPalavra;
+  }
+  
+  console.log(maiorPalavra(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'])); //Fernanda

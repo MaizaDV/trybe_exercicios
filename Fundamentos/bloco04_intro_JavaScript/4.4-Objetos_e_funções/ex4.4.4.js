da260295078fcb3d67311b3a220f91dd98efba6f
maizadv <021maizadv@gmail.com>
2022-09-27 23:33:23 -0300

    adiciona função maior que dois número e maior que três

--- Fundamentos/bloco04_intro_JavaScript/4.4-Objetos_e_funções/ex4.4.4.js
+++ Fundamentos/bloco04_intro_JavaScript/4.4-Objetos_e_funções/ex4.4.4.js
@@ -1,5 +1,6 @@
 let a = Math.floor(Math.random() * 10) + 1;
 let b = Math.floor(Math.random() * 10) + 1;
+let c = Math.floor(Math.random() * 10) + 1;
 
 function adição(a, b) {
 	return a + b;
@@ -25,3 +26,27 @@ function módulo(a, b) {
 	return a % b;
 }
 console.log("Módulo: " + a + " % " + b + " = " + módulo(a, b));
+
+function maiorNum(a, b) {
+	if (a > b) {
+		return (a + " é maior que " + b);
+	} if (a < b) {
+		return (a + " é menor que " + b);
+	} else {
+		return ("São iguais");
+	}
+}
+console.log(maiorNum(a, b));
+
+function maiorNumDeTres(a, b, c) {
+	if (a > b && a > c) {
+		return (a + " é maior que " + b + ", " + c);
+	} else if (b > a && b > c) {
+		return (b + " é maior que " + a + ", " + c);
+	} else if (c > a && c > b) {
+		return (c + " é maior que " + a + ", " + b);
+	} else {
+		return ("São iguais");
+	}
+}
+console.log(maiorNumDeTres(a, b, c));

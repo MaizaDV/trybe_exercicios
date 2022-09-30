024854d61592ebc6820a93b02c5d41b6ec47271c
maizadv <021maizadv@gmail.com>
2022-09-30 20:01:01 -0300

    altera style backgroundColor

--- Fundamentos/seção05_DOM_Eventos_e_WebStorage/Dia1-DOM_e_seletores/S-05.D-01-Seletores_de _elementos/usando_seletores/script.js
+++ Fundamentos/seção05_DOM_Eventos_e_WebStorage/Dia1-DOM_e_seletores/S-05.D-01-Seletores_de _elementos/usando_seletores/script.js
@@ -1,2 +1,3 @@
 document.getElementById("header-container").style.backgroundColor = "rgb(6,177,107)";
 document.getElementById("footer-container").style.backgroundColor = "rgb(14,47,46)";
+document.getElementById("container").style.backgroundColor = "rgb(243,243,243)";

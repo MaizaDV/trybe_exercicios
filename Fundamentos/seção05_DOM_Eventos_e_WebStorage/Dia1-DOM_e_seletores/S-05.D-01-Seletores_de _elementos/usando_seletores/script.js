82d7ca5602dde33b3f5dbad15eb7f60401585a34
maizadv <021maizadv@gmail.com>
2022-09-30 21:57:56 -0300

    altera style div background emergency-tasks

--- Fundamentos/seção05_DOM_Eventos_e_WebStorage/Dia1-DOM_e_seletores/S-05.D-01-Seletores_de _elementos/usando_seletores/script.js
+++ Fundamentos/seção05_DOM_Eventos_e_WebStorage/Dia1-DOM_e_seletores/S-05.D-01-Seletores_de _elementos/usando_seletores/script.js
@@ -1,5 +1,10 @@
 document.getElementById("header-container").style.backgroundColor = "rgb(6,177,107)";
 document.getElementById("footer-container").style.backgroundColor = "rgb(14,47,46)";
 document.getElementById("container").style.backgroundColor = "rgb(243,243,243)";
-document.getElementsByClassName('emergency-tasks')[0].style.backgroundColor = 'rgb(255, 159, 132)';
-document.getElementsByClassName('no-emergency-tasks')[0].style.backgroundColor = 'rgb(249, 219, 94)';
+document.getElementsByClassName("emergency-tasks")[0].style.backgroundColor = "rgb(255, 159, 132)";
+document.getElementsByClassName("no-emergency-tasks")[0].style.backgroundColor = "rgb(249, 219, 94)";
+
+const divEmergencyTasks = document.querySelectorAll('.emergency-tasks h3');
+for (let i = 0; i < divEmergencyTasks.length; i += 1) {
+  divEmergencyTasks[i].style.backgroundColor = "rgb(165, 0, 243)"
+};

6928acafbaebe1d0e2a6432c6982707b7b0bbd38
maizadv <021maizadv@gmail.com>
2022-09-30 21:37:16 -0300

    altera style section no-emergency-tasks

--- Fundamentos/seção05_DOM_Eventos_e_WebStorage/Dia1-DOM_e_seletores/S-05.D-01-Seletores_de _elementos/usando_seletores/script.js
+++ Fundamentos/seção05_DOM_Eventos_e_WebStorage/Dia1-DOM_e_seletores/S-05.D-01-Seletores_de _elementos/usando_seletores/script.js
@@ -2,3 +2,4 @@ document.getElementById("header-container").style.backgroundColor = "rgb(6,177,1
 document.getElementById("footer-container").style.backgroundColor = "rgb(14,47,46)";
 document.getElementById("container").style.backgroundColor = "rgb(243,243,243)";
 document.getElementsByClassName('emergency-tasks')[0].style.backgroundColor = 'rgb(255, 159, 132)';
+document.getElementsByClassName('no-emergency-tasks')[0].style.backgroundColor = 'rgb(249, 219, 94)';

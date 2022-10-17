43576d2ac75528629a4ae5eeed1e9554cbd29711
maizadv <021maizadv@gmail.com>
2022-10-17 20:11:48 -0300

    const

---
+++ Fundamentos/seção7_intro_ES6_e_Testes_Unitarios/Dia1-let_const_arrowFunctions_e_templateLiterals/01-var_let_e_const/7.1.4.constImutavel.js
@@ -0,0 +1,7 @@
+const favoriteLanguage = 'Javascript';
+favoriteLanguage = 'Python';
+console.log(favoriteLanguage); // Erro, const é imutável
+
+let favoriteTechnology = 'Machine learning';
+favoriteTechnology = 'Facial recognition';
+console.log(favoriteTechnology); // Facial recognition

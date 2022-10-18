677f8dc7909483077de6a0e87e61fd533b0bb947
maizadv <021maizadv@gmail.com>
2022-10-18 18:50:45 -0300

    arrow functions

--- Fundamentos/seção7_intro_ES6_e_Testes_Unitarios/Dia1-let_const_arrowFunctions_e_templateLiterals/01-var_let_e_const/7.1.3.varSubescreve.js
+++
@@ -1,5 +0,0 @@
-var userName = 'Isabella';
-var userName = 'Lucas';
-console.log(userName); // Resultado: Lucas
-
-// Em aplicações maiores ao usar o var, você pode redeclarar variáveis acidentalmente e nem perceber o que aconteceu. Por isso, encontrar e corrigir bugs pode se tornar uma verdadeira missão impossível. ⚠️ Por estes motivos, declarar variáveis com o var não é uma boa prática.

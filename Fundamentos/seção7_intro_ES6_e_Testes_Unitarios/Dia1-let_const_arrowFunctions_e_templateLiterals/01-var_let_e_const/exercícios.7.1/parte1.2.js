5dbdb9c5d771494c856b4d5047c3749c24bf9bd3
maizadv <021maizadv@gmail.com>
2022-10-19 13:40:43 -0300

    exercício 2, parte 1

--- Fundamentos/seção7_intro_ES6_e_Testes_Unitarios/Dia1-let_const_arrowFunctions_e_templateLiterals/01-var_let_e_const/exercícios.7.1/parte1.2.js
+++ Fundamentos/seção7_intro_ES6_e_Testes_Unitarios/Dia1-let_const_arrowFunctions_e_templateLiterals/01-var_let_e_const/exercícios.7.1/parte1.2.js
@@ -2,6 +2,7 @@ const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
 const sortOddsAndEvens = () => {
 // Seu código aqui.
-  return oddsAndEvens.sort((a, b) => b - a); // ordena de forma decrescente, se colocar a - b, ordena de forma crescente
+  oddsAndEvens.sort((a, b) => a - b); // ordena de forma crescente, se colocar b - a, ordena de forma decrescente
+  return `Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`
 }
 console.log(sortOddsAndEvens()); // será necessário alterar essa linha 😉

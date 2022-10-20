dd78800df3057980bd932fe57f1ac7c9d622ae43
maizadv <021maizadv@gmail.com>
2022-10-20 18:32:25 -0300

    atualiza exercícios seção 3

--- Fundamentos/seção4_intro_JavaScript/4.2-Array_e_LoopFor/4.2.1.js
+++ Fundamentos/seção4_intro_JavaScript/4.2-Array_e_LoopFor/4.2.1.js
@@ -19,7 +19,7 @@ for (let i =0; i < numbers.length; i += 1) {
 } if (soma/contador > 20) {
     console.log("Valor maior que 20.");
 } else {
-    console.log("Valoer menor ou igual a 20");
+    console.log("Valor menor ou igual a 20");
 };
 console.log("Soma dos valores da array: " + soma);
 console.log("A média aritmética do array: " + soma / contador);

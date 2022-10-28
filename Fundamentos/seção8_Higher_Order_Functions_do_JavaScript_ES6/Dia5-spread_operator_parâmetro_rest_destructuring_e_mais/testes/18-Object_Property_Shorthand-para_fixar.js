ad79ce21f58d2181e267d3f90b76950ed61cac2b
maizadv <021maizadv@gmail.com>
2022-10-28 20:39:06 -0300

    object property shorthand testes

---
+++ Fundamentos/seção8_Higher_Order_Functions_do_JavaScript_ES6/Dia5-spread_operator_parâmetro_rest_destructuring_e_mais/testes/18-Object_Property_Shorthand-para_fixar.js
@@ -0,0 +1,16 @@
+//? altere a função getPosition utilizando a property shorthand.
+
+// const getPosition = (latitude, longitude) => ({
+//   latitude: latitude,
+//   longitude: longitude,
+// });
+
+// console.log(getPosition(-19.8157, -43.9542));
+
+// ? property shorthand.
+const getPosition = (latitude, longitude) => ({
+  latitude,
+  longitude,
+});
+
+console.log(getPosition(-19.8157, -43.9542));

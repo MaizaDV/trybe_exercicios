08d50ba4683c00eb3bb9b6b3d57173ee802917ce
maizadv <021maizadv@gmail.com>
2022-10-28 00:16:36 -0300

    execício 1

--- Fundamentos/seção8_Higher_Order_Functions_do_JavaScript_ES6/Dia2-forEach_find_some_every_sort/exercícios.8.2/01-find_name.js
+++ Fundamentos/seção8_Higher_Order_Functions_do_JavaScript_ES6/Dia2-forEach_find_some_every_sort/exercícios.8.2/01-find_name.js
@@ -3,5 +3,7 @@ const books = require('./books')
 //? Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947.
 
 function authorBornIn1947() {
-  // escreva aqui o seu código
+  return books.find((book) => book.author.birthYear === 1947).author.name;
 }
+
+console.log(authorBornIn1947());

74c1e8fb0f77516688f7ccd92432109e83966b83
maizadv <021maizadv@gmail.com>
2022-10-28 19:42:04 -0300

    array destructuring testes

--- Fundamentos/seção8_Higher_Order_Functions_do_JavaScript_ES6/Dia5-spread_operator_parâmetro_rest_destructuring_e_mais/testes/13-Array_Destructuring-para_fixar2.js
+++ Fundamentos/seção8_Higher_Order_Functions_do_JavaScript_ES6/Dia5-spread_operator_parâmetro_rest_destructuring_e_mais/testes/13-Array_Destructuring-para_fixar2.js
@@ -3,6 +3,14 @@ let animal = 'água';
 let bebida = 'arroz';
 
 console.log(comida, animal, bebida); // arroz gato água
+console.log('=> ---------------------');
 
 // Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
 
+const array = [bebida, comida, animal];
+const [arroz, gato, agua] = array
+console.log(arroz, gato, agua);
+
+// outro modo de fazer:
+// [comida, animal, bebida] = [bebida, comida, animal]; // Criando o array e o desestruturando
+// console.log(comida, animal, bebida);

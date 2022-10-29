a095284dbc223fc5ae1500b16490fadd91acd780
maizadv <021maizadv@gmail.com>
2022-10-29 16:37:49 -0300

    data bônus

---
+++ Fundamentos/seção8_Higher_Order_Functions_do_JavaScript_ES6/Dia1- Introdução_a_Higher Order_Functions/exercícios.8.1/Bônus/battleMembersData.js
@@ -0,0 +1,23 @@
+const mage = {
+  healthPoints: 130,
+  intelligence: 45,
+  mana: 125,
+  damage: undefined,
+};
+
+const warrior = {
+  healthPoints: 200,
+  strength: 30,
+  weaponDmg: 2,
+  damage: undefined,
+};
+
+const dragon = {
+  healthPoints: 350,
+  strength: 50,
+  damage: undefined,
+};
+
+const battleMembers = { mage, warrior, dragon };
+
+module.exports = battleMembers;

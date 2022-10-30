96426833ada8504bb9aecff29aad1d07ca6ef129
maizadv <021maizadv@gmail.com>
2022-10-29 22:32:54 -0300

    exercício 5 e 6

--- Fundamentos/seção8_Higher_Order_Functions_do_JavaScript_ES6/Dia2-forEach_find_some_every_sort/exercícios.8.2/05-everyoneWasBornOnSecXX.js
+++ Fundamentos/seção8_Higher_Order_Functions_do_JavaScript_ES6/Dia2-forEach_find_some_every_sort/exercícios.8.2/05-everyoneWasBornOnSecXX.js
@@ -6,5 +6,7 @@ const books = require('./books')
 //* const expectedResult = false;
 
 function everyoneWasBornOnSecXX() {
-  // escreva seu código aqui
+  return books.every((element) => element.author.birthYear > 1900 && element.author.birthYear <= 2000)
 }
+
+console.log(everyoneWasBornOnSecXX());
--- Fundamentos/seção8_Higher_Order_Functions_do_JavaScript_ES6/Dia2-forEach_find_some_every_sort/exercícios.8.2/06-someBookWasReleaseOnThe80s.js
+++ Fundamentos/seção8_Higher_Order_Functions_do_JavaScript_ES6/Dia2-forEach_find_some_every_sort/exercícios.8.2/06-someBookWasReleaseOnThe80s.js
@@ -5,5 +5,8 @@ const books = require('./books')
 //* const expectedResult = true;
 
 function someBookWasReleaseOnThe80s() {
-  // escreva seu código aqui
+  return books.some((element) =>
+    element.releaseYear >= 1980 && element.releaseYear <= 1990)
 }
+
+console.log(someBookWasReleaseOnThe80s());

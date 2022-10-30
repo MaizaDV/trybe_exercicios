ff958feced402054d5b181379d855fd7f5d3f4f4
maizadv <021maizadv@gmail.com>
2022-10-29 21:46:07 -0300

    exercício 4

--- Fundamentos/seção8_Higher_Order_Functions_do_JavaScript_ES6/Dia2-forEach_find_some_every_sort/exercícios.8.2/04-booksOrderedByReleaseYearDesc.js
+++ Fundamentos/seção8_Higher_Order_Functions_do_JavaScript_ES6/Dia2-forEach_find_some_every_sort/exercícios.8.2/04-booksOrderedByReleaseYearDesc.js
@@ -48,5 +48,7 @@ const books = require('./books')
 //* ];
 
 function booksOrderedByReleaseYearDesc() {
-  // escreva aqui seu código
+  return books.sort((a, b) => b.releaseYear - a.releaseYear);
 }
+
+console.log(booksOrderedByReleaseYearDesc());

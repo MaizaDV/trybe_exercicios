6ca40fc8f740a12a2eb785cc4988ac4e54e02154
maizadv <021maizadv@gmail.com>
2022-11-15 17:17:08 -0300

    Código assíncrono

---
+++ Front_end/seção1_Intro_ao _front-end_e_JavaScript_assíncrono/Dia2-JavaScript_Assíncrono-Promises_e_fetch/testes/03-construindo_uma_Promise.js
@@ -0,0 +1,7 @@
+//? estrutura da promise
+new Promise((resolve, reject) => {});
+
+// Os parâmetros resolve e reject serão usados para definirmos qual o estado que promise terá quando for finalizada.
+
+// Quando a promise for resolvida, usamos a função resolve;
+// Quando a promise for rejeitada, usamos a função reject.
---
+++ Front_end/seção1_Intro_ao _front-end_e_JavaScript_assíncrono/Dia2-JavaScript_Assíncrono-Promises_e_fetch/testes/04-Promises.js
@@ -0,0 +1,28 @@
+//! fazer testes no console do navegador, não pelo navegador
+
+//? função que gera um número aleatório
+const generateRandomNumber = () => Math.round(Math.random() * 10);
+
+//? promise resolvida retornando o número aleatório
+const resolvedPromise = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const randomNumber = generateRandomNumber();
+      resolve(randomNumber);
+    }, 1000);
+  });
+console.log(resolvedPromise())
+
+//? promise rejeitada retornando um objeto de erro
+const rejectedPromise = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const randomNumber = generateRandomNumber();
+      reject(new Error(`O número ${randomNumber} é inválido.`));
+  }, 1000);
+});
+console.log(rejectedPromise());
+
+// Anota aí 📝: Repare que podemos retornar qualquer valor usando a função reject, mas geralmente é uma boa prática retornar um objeto de erro.
+
+

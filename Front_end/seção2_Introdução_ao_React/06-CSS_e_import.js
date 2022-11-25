665364a96a987b966dea370ba65e6e4e470202b1
maizadv <021maizadv@gmail.com>
2022-11-24 23:11:25 -0300

    usando CSS e import na pasta 04-react-dom-elements

--- Front_end/seção2_Introdução_ao_React/06-CSS_e_import.js
+++ Front_end/seção2_Introdução_ao_React/06-CSS_e_import.js
@@ -11,14 +11,14 @@ import Header from './components/Header.js' // importando de um caminho relativo
 
 // A utilização de CSS (estilização) em componentes React não é nada muito diferente de como é feito no HTML, você deve criar um arquivo para manter todo o seu CSS e então deverá importá-lo onde você deseja utilizá-lo e colocar as classes e ids que você criou nos elementos da sua página.
 
-// => exemplo:
-/* App.css */
+// => exemplo (usado na pasta 04-react-dom-elements):
+/* caminho 04-react-dom-elements/src/App.css */
 /* .App {
   background-color: #282c34;
   text-align: center;
 } */
 
-// App.js
+// caminho 04-react-dom-elements/src/App.js
 import React from 'react';
 import './App.css';
 

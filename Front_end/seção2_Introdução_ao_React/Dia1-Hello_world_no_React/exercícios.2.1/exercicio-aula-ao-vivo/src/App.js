e72f38cc761d724f15d83e43e6a52aefe5bf308a
maizadv <021maizadv@gmail.com>
2022-11-26 16:33:25 -0300

    pastas do exercício

--- Front_end/seção2_Introdução_ao_React/Dia1-Hello_world_no_React/exercícios.2.1/exercicio-aula-ao-vivo/src/App.js
+++ Front_end/seção2_Introdução_ao_React/Dia1-Hello_world_no_React/exercícios.2.1/exercicio-aula-ao-vivo/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const data = {
+  name: 'Jhon Connor',
+  module: 'Front-end',
+  bornIn: 1989,
+  watchedMovies: [
+    { title: 'Titanic', release: 2000, favorite: true },
+    { title: 'Terminator Salvation', release: 2009, favorite: true },
+    { title: 'The Lord of The Rings', release: 2002, favorite: true },
+    { title: 'Helloween Kills', release: 2022, favorite: false },
+  ],
+};
+
+// D E S A F I O
+
+// exibir todos os dados acima na tela (name, module, bornIn, watchedMovies)
+// exibir apenas os dados referentes aos filmes favoritados (favorite === true)
+// se o ano de nascimento for <= 2004 o nome da pessoa deve ficar verde, senão vermelho
+
+class App extends React.Component {
+  render() {
+    console.log('teste aqui 12312313');
+    return (
+      <div>
+        <h1>Opa, alô Trybers</h1>
+        <p>Bora começar? Valendooooo ...</p>
+      </div>
+    );
+  }
 }
 
 export default App;

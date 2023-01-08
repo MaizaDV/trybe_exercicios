4158c40f319ec343eeaa5888d3306d9a8c43325c
maiza <021maizadv@gmail.com>
2023-01-08 11:53:28 -0300

    commit inicial

---
+++ Front_end/secao4_Ciclo_de_Vida_de_Componentes_e_React_Router/Dia1-Ciclo_de_vida_de_componentes/testes/01-entendendo-quando-cada-metodo-chamado/src/components/Counter.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+    console.log("construtor");
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("shouldComponentUpdate");
+    return true;
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("componentDidUpdate");
+  }
+
+  render() {
+    console.log("render");
+    return (
+      <div>
+        <p>Contador</p>
+        <button
+          onClick={() => this.setState((state) => ({ counter: state.counter + 1 }))}
+        >
+          Soma
+        </button>
+        <p>{this.state.counter}</p>
+      </div>
+    );
+  }
+}
+
+export default Counter;

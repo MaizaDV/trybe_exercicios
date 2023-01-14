943bc4a8b22c99b1a01005cfd1c863dc68b4bc32
maiza <021maizadv@gmail.com>
2023-01-14 17:00:11 -0300

    usando React Router

---
+++ Front_end/secao4_Ciclo_de_Vida_de_Componentes_e_React_Router/Dia2-React_Router/testes/01-react-router-dom/src/components/About.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class About extends Component {
+  render() {
+    return (
+      <span>Se meu navio de React é o de Teseu e eu refatorei todos os seus componentes, ele ainda é o meu navio?</span>
+    );
+  }
+}
+
+export default About;

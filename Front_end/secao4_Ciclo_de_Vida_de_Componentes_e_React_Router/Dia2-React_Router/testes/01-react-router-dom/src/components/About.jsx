377a79be8c932e80c308facb41d7d69176c5bc77
maiza <021maizadv@gmail.com>
2023-01-14 17:28:14 -0300

    usando link no React Router

--- Front_end/secao4_Ciclo_de_Vida_de_Componentes_e_React_Router/Dia2-React_Router/testes/01-react-router-dom/src/components/About.jsx
+++ Front_end/secao4_Ciclo_de_Vida_de_Componentes_e_React_Router/Dia2-React_Router/testes/01-react-router-dom/src/components/About.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class About extends Component {
   render() {
     return (
-      <span>Se meu navio de React é o de Teseu e eu refatorei todos os seus componentes, ele ainda é o meu navio?</span>
+      <>
+        <Link to="/">Voltar à Home</Link>
+        <span>Se meu navio de React é o de Teseu e eu refatorei todos os seus componentes, ele ainda é o meu navio?</span>
+      </>
     );
   }
 }

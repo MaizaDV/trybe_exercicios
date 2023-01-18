8428c19f04e2f5bdfcebfe38ed5bb3a94effcd16
maiza <021maizadv@gmail.com>
2023-01-18 20:08:17 -0300

    teste de email da aplicação

--- Front_end/Seção6_Testes_automatizados_com_React_Testing_Library/Dia1-RTL-Primeiros_passos/testes/01-rtl-na-pratica/src/App.test.js
+++ Front_end/Seção6_Testes_automatizados_com_React_Testing_Library/Dia1-RTL-Primeiros_passos/testes/01-rtl-na-pratica/src/App.test.js
@@ -1,8 +1,21 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Verifica se existe um input do tipo email na tela', () => {
+  // acessar os elementos da tela
+  render(<App />) // a função render() recebe qual componente vai ser renderizado
+  const inputEmail = screen.getByLabelText("Email");
+
+  // fazer os testes
+  expect(inputEmail).toBeInTheDocument();
+  expect(inputEmail.type).toBe("email");
+
 });
+
+  // todos os testes seguem mais ou menos essas etapas:
+  // acessar os elementos da tela
+
+  // interagir como os elementos (se for necessário)
+
+  // fazer os testes

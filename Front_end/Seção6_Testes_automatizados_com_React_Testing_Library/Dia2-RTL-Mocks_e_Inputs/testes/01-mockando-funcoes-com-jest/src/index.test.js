d4b73748900b71f4b4ae721f8ff42188b512e339
maiza <021maizadv@gmail.com>
2023-01-24 04:11:13 -0300

    Mockando funções com Jest 2

--- Front_end/Seção6_Testes_automatizados_com_React_Testing_Library/Dia2-RTL-Mocks_e_Inputs/testes/01-mockando-funcoes-com-jest/src/index.test.js
+++ Front_end/Seção6_Testes_automatizados_com_React_Testing_Library/Dia2-RTL-Mocks_e_Inputs/testes/01-mockando-funcoes-com-jest/src/index.test.js
@@ -43,3 +43,44 @@ describe('Aprendendo sobre Mocks', () => {
   });
 
 });
+
+describe('Aprendendo sobre mocks', () => {
+  test("Testando se a função foi chamada.", () => {
+    // testando se a função foi chamada. Não simulamos nenhum comportamento aqui, pois, para esse teste, isso não importa! Apenas queremos saber se ela foi chamada.
+    divisivelPorDois = jest.fn();
+  
+    divisivelPorDois();
+    expect(divisivelPorDois).toHaveBeenCalled();
+  });
+
+//...
+  test("testando se a função foi chamada e qual seu retorno", () => {
+    divisivelPorDois = jest.fn().mockReturnValue(true);
+
+    divisivelPorDois();
+    expect(divisivelPorDois).toHaveBeenCalled();
+    expect(divisivelPorDois()).toBe(true);
+  });
+
+  // ...
+  test("testando quantas vezes a função foi chamada e qual seu retorno", () => {
+
+    divisivelPorDois = jest
+      .fn()
+      .mockReturnValue('default value')
+      .mockReturnValueOnce('first call')
+      .mockReturnValueOnce('second call');
+
+    expect(divisivelPorDois).toHaveBeenCalledTimes(0);
+
+    expect(divisivelPorDois()).toBe("first call");
+    expect(divisivelPorDois).toHaveBeenCalledTimes(1);
+
+    expect(divisivelPorDois()).toBe("second call");
+    expect(divisivelPorDois).toHaveBeenCalledTimes(2);
+
+    expect(divisivelPorDois()).toBe("default value");
+    expect(divisivelPorDois).toHaveBeenCalledTimes(3);
+  });
+
+});

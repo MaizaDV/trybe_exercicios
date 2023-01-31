29ddd9cc59f7e1e6eaf2a4ec8ccff9ba2b16cd2a
maiza <021maizadv@gmail.com>
2023-01-31 00:59:41 -0300

    cria estado inicial do redux

--- Front_end/seção7_Gerenciamento_de_estado_com_Redux/Dia1-Introducao_ao_Redux-O_estado_global_da_aplicacao/testes/01-Usando_Redux_na_pratica/index.js
+++ Front_end/seção7_Gerenciamento_de_estado_com_Redux/Dia1-Introducao_ao_Redux-O_estado_global_da_aplicacao/testes/01-Usando_Redux_na_pratica/index.js
@@ -1,3 +1,12 @@
+import { legacy_createStore as createStore } from "redux";
+
+const INITIAL_STATE = { count: 0 };
+
+const reducer = (state = INITIAL_STATE, action) => {
+  return state;
+};
+
+const store = createStore(reducer); // para criar a store, precisa ser chamado o reducer
 
 const incrementBtn = document.querySelector("button");
 incrementBtn.addEventListener("click", () => {

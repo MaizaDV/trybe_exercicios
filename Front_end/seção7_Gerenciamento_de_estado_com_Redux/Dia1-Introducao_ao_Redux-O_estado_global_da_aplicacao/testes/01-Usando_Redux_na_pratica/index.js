0a2caa1856ee24b8cd39e3ba1377c6e4d3242705
maiza <021maizadv@gmail.com>
2023-01-31 13:16:44 -0300

    cria uma action

--- Front_end/seção7_Gerenciamento_de_estado_com_Redux/Dia1-Introducao_ao_Redux-O_estado_global_da_aplicacao/testes/01-Usando_Redux_na_pratica/index.js
+++ Front_end/seção7_Gerenciamento_de_estado_com_Redux/Dia1-Introducao_ao_Redux-O_estado_global_da_aplicacao/testes/01-Usando_Redux_na_pratica/index.js
@@ -1,14 +1,25 @@
 import { legacy_createStore as createStore } from "redux";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
+ // toda aplicação quando ela começa, tem um estado inicial, é o primeiro estado que começa com a aplicação
 const INITIAL_STATE = { count: 0 };
 
+// reducer-função responsável por alterar o estado global
 const reducer = (state = INITIAL_STATE, action) => {
+  if (action.type === 'INCREMENT_COUNTER') {
+    console.log(state);
+    return { count: state.count + 1 };
+  }
   return state;
 };
 
-const store = createStore(reducer); // para criar a store, precisa ser chamado o reducer
+// para criar a store, precisa ser chamado o reducer
+const store = createStore(reducer, composeWithDevTools());
+
+// toda vez que a gente for chamar uma ação, a gente precisa passar para o reducer qual a ação que foi chamada, a ação precisa de um identificador para cada ação da nossa aplicação
+const action = {type: 'INCREMENT_COUNTER'}
 
 const incrementBtn = document.querySelector("button");
 incrementBtn.addEventListener("click", () => {
-  console.log("click");
+  store.dispatch(action); // dispara a action
 })

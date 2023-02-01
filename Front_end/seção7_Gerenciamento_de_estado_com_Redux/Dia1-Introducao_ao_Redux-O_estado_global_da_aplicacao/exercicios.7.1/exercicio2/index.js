a843ba64cf060c7115a290d1282cf4f60179e387
maiza <021maizadv@gmail.com>
2023-02-01 18:45:59 -0300

    configuração inicial exercício 2

---
+++ Front_end/seção7_Gerenciamento_de_estado_com_Redux/Dia1-Introducao_ao_Redux-O_estado_global_da_aplicacao/exercicios.7.1/exercicio2/index.js
@@ -0,0 +1,15 @@
+const INITIAL_STATE = {
+  status: 'offline',
+  theme: 'dark',
+};
+
+const themeButton = document.getElementById('toggle-theme');
+const statusButton = document.getElementById('toggle-status');
+
+themeButton.addEventListener('click', () => {
+  // coloque o código aqui.
+});
+
+statusButton.addEventListener('click', () => {
+  // coloque o código aqui.
+});

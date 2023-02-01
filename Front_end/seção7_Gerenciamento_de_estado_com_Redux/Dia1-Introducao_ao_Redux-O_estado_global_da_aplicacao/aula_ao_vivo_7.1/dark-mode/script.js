3fdaadd9eee372366929cdf1c5ec153ebd3694f1
maiza <021maizadv@gmail.com>
2023-01-31 22:20:37 -0300

    atualiza o theme to dark

--- Front_end/seção7_Gerenciamento_de_estado_com_Redux/Dia1-Introducao_ao_Redux-O_estado_global_da_aplicacao/aula_ao_vivo_7.1/dark-mode/script.js
+++ Front_end/seção7_Gerenciamento_de_estado_com_Redux/Dia1-Introducao_ao_Redux-O_estado_global_da_aplicacao/aula_ao_vivo_7.1/dark-mode/script.js
@@ -1,12 +1,34 @@
+const CHANGE_TO_DARK = 'CHANGE_TO_DARK';
+
+// actions e os dispatchs
+const actionChangeToDark = () => {
+  console.log('Clicou');
+  store.dispatch({ type: CHANGE_TO_DARK });
+};
+
+const toggleButton = document.querySelector('#light-switch');
+toggleButton.addEventListener('click', actionChangeToDark);
+
+// reducer e seu estado inicial
 const INITIAL_STATE = {
   theme: 'light',
 };
 
 const reducer = (state = INITIAL_STATE, action) => {
-  return state;
+  console.log('state', state);
+  console.log('action', action);
+
+  switch (action.type) {
+    case CHANGE_TO_DARK:
+      // Montar a minha atualização
+      return { theme: 'dark' };
+  
+    default:
+      return state;
+  }
 };
 
 // Eu estou criando a minha store para ser utilizada posteriormente.
 const store = Redux.createStore(reducer); // local que vai ser armazenado.
+// console.log(store);
 
-console.log(store);

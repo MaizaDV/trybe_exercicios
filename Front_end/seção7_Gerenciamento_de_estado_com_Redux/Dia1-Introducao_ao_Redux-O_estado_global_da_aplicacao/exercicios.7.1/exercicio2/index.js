f4eb5a3e0be424a6a353df7a4716f595d250227f
maiza <021maizadv@gmail.com>
2023-02-01 19:33:52 -0300

    exercício 2 feito

--- Front_end/seção7_Gerenciamento_de_estado_com_Redux/Dia1-Introducao_ao_Redux-O_estado_global_da_aplicacao/exercicios.7.1/exercicio2/index.js
+++ Front_end/seção7_Gerenciamento_de_estado_com_Redux/Dia1-Introducao_ao_Redux-O_estado_global_da_aplicacao/exercicios.7.1/exercicio2/index.js
@@ -3,13 +3,66 @@ const INITIAL_STATE = {
   theme: 'dark',
 };
 
-const themeButton = document.getElementById('toggle-theme');
+const CHANGE_THEME = 'CHANGE_THEME';
+const CHANGE_STATUS = 'CHANGE_STATUS';
+
+const actionChangeStatus = () => {
+  store.dispatch({ type: CHANGE_STATUS });
+};
+
+const actionChangeTheme = () => {
+  store.dispatch({ type: CHANGE_THEME });
+};
+
+const reducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case CHANGE_STATUS:
+      const newStatus = state.status === 'offline' ? 'online' : 'offline';
+      return {
+        ...state, 
+        status: newStatus 
+      };
+
+    case CHANGE_THEME:
+      const newTheme = state.theme === 'dark' ? 'light' : 'dark';
+      return { 
+        ...state,
+        theme: newTheme
+      };
+
+    default:
+      return state;
+  }
+};
+
+const store = Redux.createStore(reducer);
+
 const statusButton = document.getElementById('toggle-status');
+statusButton.addEventListener('click', actionChangeStatus);
 
-themeButton.addEventListener('click', () => {
-  // coloque o código aqui.
-});
+const themeButton = document.getElementById('toggle-theme');
+themeButton.addEventListener('click', actionChangeTheme);
 
-statusButton.addEventListener('click', () => {
-  // coloque o código aqui.
+store.subscribe(() => {
+  const state = store.getState();
+  const body = document.querySelector('body');
+  const status = document.getElementById('status');
+
+  if (state.theme === 'light') {
+    themeButton.innerText = 'Dark Mode';
+    body.style.backgroundColor = '#fff';
+    body.style.color = '#333';
+  } else {
+    themeButton.innerText = 'Light Mode';
+    body.style.backgroundColor = '#333';
+    body.style.color = '#fff';
+  }
+
+  if (state.status === 'online') {
+    statusButton.innerText = 'Ficar Offline';
+    status.innerText = 'Online';
+  } else {
+    statusButton.innerText = 'Ficar Online';
+    status.innerText = 'Offline';
+  }
 });

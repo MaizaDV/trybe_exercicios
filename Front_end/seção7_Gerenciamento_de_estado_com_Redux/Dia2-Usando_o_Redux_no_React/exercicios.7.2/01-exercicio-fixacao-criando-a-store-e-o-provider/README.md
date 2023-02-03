8ab5f577e7de257415d269228902a008ca0d6b97
maiza <021maizadv@gmail.com>
2023-02-03 14:12:02 -0300

    01 exercício de fixação - Criando a Store e o Provider - feito

--- Front_end/seção7_Gerenciamento_de_estado_com_Redux/Dia2-Usando_o_Redux_no_React/exercicios.7.2/01-exercicio-fixacao-criando-a-store-e-o-provider/README.md
+++ Front_end/seção7_Gerenciamento_de_estado_com_Redux/Dia2-Usando_o_Redux_no_React/exercicios.7.2/01-exercicio-fixacao-criando-a-store-e-o-provider/README.md
@@ -17,4 +17,4 @@ Para a conclusão deste exercício, você precisa alterar apenas os arquivos da 
 
 Exercício criado com o intuito de praticar os ensinamentos de React utilizando Redux.
 
-<!-- https://stackblitz.com/edit/react-cvmjpe?file=src/redux/index.js -->
+< <https://stackblitz.com/edit/react-cvmjpe?file=src/redux/index.js> >
--- Front_end/seção7_Gerenciamento_de_estado_com_Redux/Dia2-Usando_o_Redux_no_React/exercicios.7.2/01-exercicio-fixacao-criando-a-store-e-o-provider/src/redux/index.js
+++ Front_end/seção7_Gerenciamento_de_estado_com_Redux/Dia2-Usando_o_Redux_no_React/exercicios.7.2/01-exercicio-fixacao-criando-a-store-e-o-provider/src/redux/index.js
@@ -1,3 +1,12 @@
 // Ops! A aplicação está quebrada!!
 // Implemente a Store nesse arquivo e o Provider no arquivo ./src/index.js
 // para a aplicação voltar a funcionar!
+
+import { legacy_createStore as createStore } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
+
+import defeatSaga from './reducers/cosmoReducer'; 
+
+const store = createStore(defeatSaga, composeWithDevTools());
+
+export default store;

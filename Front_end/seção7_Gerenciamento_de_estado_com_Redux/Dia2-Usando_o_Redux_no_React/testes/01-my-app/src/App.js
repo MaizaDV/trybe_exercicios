a559d1796b6e4c0120e4a3bad8c1f64f74afb594
maiza <021maizadv@gmail.com>
2023-02-03 01:47:12 -0300

    testa imutabilidade do estado global

--- Front_end/seção7_Gerenciamento_de_estado_com_Redux/Dia2-Usando_o_Redux_no_React/testes/01-my-app/src/App.js
+++ Front_end/seção7_Gerenciamento_de_estado_com_Redux/Dia2-Usando_o_Redux_no_React/testes/01-my-app/src/App.js
@@ -1,17 +1,24 @@
 import './App.css';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { actionCreator } from './redux/actions/index';
+import { actionCreator, clickCounter } from './redux/actions/index';
 
 class App extends Component {
   render() {
-    const { dispatch, countState } = this.props;
+    const { dispatch, countState, clicksCount } = this.props;
+
+    const dispatchAll = (add = 1) => {
+      dispatch(actionCreator(add));
+      dispatch(clickCounter());
+    };
+
     return (
       <div>
         <h1>Contador</h1>
-        <h2>{ countState }</h2>
-        <button onClick={ () => dispatch(actionCreator()) }>Incrementa 1</button>
-        <button onClick={ () => dispatch(actionCreator(5)) }>Incrementa 5</button>
+        <h2>{countState}</h2>
+        <button onClick={() => dispatchAll()}>Incrementa 1</button>
+        <button onClick={() => dispatchAll(5)}>Incrementa 5</button>
+        <h3>Número de clicks: {clicksCount}</h3>
       </div>
     );
   }
@@ -19,6 +26,7 @@ class App extends Component {
 
 const mapStateToProps = (state) => ({
   countState: state.count,
+  clicksCount: state.clicks,
 });
 
 export default connect(mapStateToProps)(App);
--- Front_end/seção7_Gerenciamento_de_estado_com_Redux/Dia2-Usando_o_Redux_no_React/testes/01-my-app/src/redux/actions/index.js
+++ Front_end/seção7_Gerenciamento_de_estado_com_Redux/Dia2-Usando_o_Redux_no_React/testes/01-my-app/src/redux/actions/index.js
@@ -1,8 +1,14 @@
 // ./src/redux/actions/index.js
 
 const INCREMENT_COUNTER = 'INCREMENT_COUNTER';
+const INCREMENT_CLICK = 'INCREMENT_CLICK';
 
 export const actionCreator = (increment = 1) => ({ 
   type: INCREMENT_COUNTER,
   payload: increment,
 });
+
+export const clickCounter = () => ({
+  type: INCREMENT_CLICK,
+});
+

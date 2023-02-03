4694da6229d949e1e2f24833a650922c86ffdb16
maiza <021maizadv@gmail.com>
2023-02-03 00:08:23 -0300

    ajusta a Store

---
+++ Front_end/seção7_Gerenciamento_de_estado_com_Redux/Dia2-Usando_o_Redux_no_React/testes/01-my-app/src/redux/store/index.js
@@ -0,0 +1,8 @@
+import { legacy_createStore as createStore } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
+
+import counterReducer from './reducers/counterReducer';  // função reducer
+
+const store = createStore(counterReducer, composeWithDevTools());
+
+export default store;

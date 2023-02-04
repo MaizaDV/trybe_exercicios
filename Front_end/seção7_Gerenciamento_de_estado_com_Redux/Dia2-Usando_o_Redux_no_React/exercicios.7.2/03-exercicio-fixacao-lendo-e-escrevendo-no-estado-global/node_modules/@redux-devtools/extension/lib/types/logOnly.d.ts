f3b32780c9bb2a4f20f55a8b01175383ae921a8f
maiza <021maizadv@gmail.com>
2023-02-04 17:02:11 -0300

    corrige erros

--- Front_end/seção7_Gerenciamento_de_estado_com_Redux/Dia2-Usando_o_Redux_no_React/exercicios.7.2/03-exercicio-fixacao-lendo-e-escrevendo-no-estado-global/node_modules/@redux-devtools/extension/lib/types/logOnly.d.ts
+++
@@ -1,5 +0,0 @@
-import type { StoreEnhancer } from 'redux';
-import type { Config, EnhancerOptions, InferComposedStoreExt } from './index';
-export declare function composeWithDevTools(config: Config): <StoreEnhancers extends readonly StoreEnhancer<unknown>[]>(...funcs: StoreEnhancers) => StoreEnhancer<InferComposedStoreExt<StoreEnhancers>>;
-export declare function composeWithDevTools<StoreEnhancers extends readonly StoreEnhancer<unknown>[]>(...funcs: StoreEnhancers): StoreEnhancer<InferComposedStoreExt<StoreEnhancers>>;
-export declare const devToolsEnhancer: (options?: EnhancerOptions) => StoreEnhancer;
--- Front_end/seção7_Gerenciamento_de_estado_com_Redux/Dia2-Usando_o_Redux_no_React/testes/01-my-app/node_modules/@redux-devtools/extension/lib/types/logOnlyInProduction.d.ts
+++
@@ -1,4 +0,0 @@
-import type { StoreEnhancer } from 'redux';
-import type { EnhancerOptions, ReduxDevtoolsExtensionCompose } from './index';
-export declare const composeWithDevTools: ReduxDevtoolsExtensionCompose;
-export declare const devToolsEnhancer: (options?: EnhancerOptions) => StoreEnhancer;

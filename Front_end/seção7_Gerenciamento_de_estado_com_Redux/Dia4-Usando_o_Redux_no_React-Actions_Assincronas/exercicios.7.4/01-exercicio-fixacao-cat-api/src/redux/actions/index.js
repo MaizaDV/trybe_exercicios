45c768aada9d0970c7cda46deb09d8eb039d867a
maiza <021maizadv@gmail.com>
2023-02-06 19:54:02 -0300

    exercício de fixação cat API - feito

--- Front_end/seção7_Gerenciamento_de_estado_com_Redux/Dia4-Usando_o_Redux_no_React-Actions_Assincronas/exercicios.7.4/01-exercicio-fixacao-cat-api/src/redux/actions/index.js
+++ Front_end/seção7_Gerenciamento_de_estado_com_Redux/Dia4-Usando_o_Redux_no_React-Actions_Assincronas/exercicios.7.4/01-exercicio-fixacao-cat-api/src/redux/actions/index.js
@@ -7,4 +7,14 @@ export const getPicture = (data) => ({ type: GET_PICTURE, data });
 
 export function fetchAPI() {
   // Desenvolva aqui o código da action assíncrona
+  return async (dispatch) => {
+    try {
+      dispatch(requestAPI());
+      const response = await fetch('https://aws.random.cat/meow');
+      const data = await response.json();
+      dispatch(getPicture(data));
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
--- Front_end/seção7_Gerenciamento_de_estado_com_Redux/Dia4-Usando_o_Redux_no_React-Actions_Assincronas/exercicios.7.4/01-exercicio-fixacao-cat-api/src/redux/store.js
+++ Front_end/seção7_Gerenciamento_de_estado_com_Redux/Dia4-Usando_o_Redux_no_React-Actions_Assincronas/exercicios.7.4/01-exercicio-fixacao-cat-api/src/redux/store.js
@@ -1,7 +1,10 @@
-import { legacy_createStore as createStore } from 'redux';
+import { applyMiddleware, legacy_createStore as createStore } from 'redux';import { composeWithDevTools } from '@redux-devtools/extension';
+import thunk from 'redux-thunk';
+import gallery from './reducers/gallery';
 
-import reducer from './reducers/gallery';
-
-const store = createStore(reducer);
+const store = createStore(
+  gallery,
+  composeWithDevTools(applyMiddleware(thunk)),
+);
 
 export default store;

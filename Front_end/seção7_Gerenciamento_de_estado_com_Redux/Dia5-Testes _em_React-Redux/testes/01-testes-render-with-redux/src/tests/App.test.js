bbf5d49c9ba83125791846db5aa5db98da6186ae
maiza <021maizadv@gmail.com>
2023-02-12 19:44:25 -0300

    separa a função renderWithRedux

--- Front_end/seção7_Gerenciamento_de_estado_com_Redux/Dia5-Testes _em_React-Redux/testes/01-testes-render-with-redux/src/tests/App.test.js
+++ Front_end/seção7_Gerenciamento_de_estado_com_Redux/Dia5-Testes _em_React-Redux/testes/01-testes-render-with-redux/src/tests/App.test.js
@@ -1,20 +1,8 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { combineReducers, legacy_createStore as createStore } from 'redux';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 
-import counterReducer from '../redux/reducers/counterReducer';
+import renderWithRedux from './helpers/renderWithRedux';
 import App from '../App';
-
-const renderWithRedux = (
-  component,
-    {
-      initialState,
-      store = createStore(combineReducers({ counterReducer }), initialState)
-    } = {}) => ({
-  ...render(<Provider store={ store }>{ component }</Provider>),
-  store,
-});
 
 test('A página deve renderizar dois botões e o número "0"', () => {
   renderWithRedux(<App />);

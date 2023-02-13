0d2808d9edb74175dc67a14c7331345b90d15cda
maiza <021maizadv@gmail.com>
2023-02-13 03:06:01 -0300

    configuração inicial 02-testando-aplicacoes-com-rotas-e-redux

--- Front_end/seção7_Gerenciamento_de_estado_com_Redux/Dia5-Testes _em_React-Redux/testes/02-testando-aplicacoes-com-rotas-e-redux/src/redux/index.js
+++ Front_end/seção7_Gerenciamento_de_estado_com_Redux/Dia5-Testes _em_React-Redux/testes/02-testando-aplicacoes-com-rotas-e-redux/src/redux/index.js
@@ -1,7 +1,8 @@
 import { legacy_createStore as createStore } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
 
 import userReducer from './reducers/userReducer';
 
-const store = createStore(userReducer);
+const store = createStore(userReducer, composeWithDevTools());
 
 export default store;

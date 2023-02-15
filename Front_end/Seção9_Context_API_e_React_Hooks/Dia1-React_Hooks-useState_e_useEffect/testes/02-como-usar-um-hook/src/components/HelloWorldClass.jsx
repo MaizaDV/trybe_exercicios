6fed35b2975bb00e669f6139866abb571617e4ef
maiza <021maizadv@gmail.com>
2023-02-15 19:04:53 -0300

    uso errado de hooks

---
+++ Front_end/Seção9_Context_API_e_React_Hooks/Dia1-React_Hooks-useState_e_useEffect/testes/02-como-usar-um-hook/src/components/HelloWorldClass.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class HelloWorldClass extends Component {
+  render() {
+    return (
+      <h1>Hello World! - Componente de Classe</h1>
+    );
+  }
+}
+
+export default HelloWorldClass;

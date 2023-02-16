79c236adef8fb555c28d012418d04bf953588e38
maiza <021maizadv@gmail.com>
2023-02-16 09:42:07 -0300

    entendeno o useEffect

--- Front_end/Seção9_Context_API_e_React_Hooks/Dia1-React_Hooks-useState_e_useEffect/testes/04-entendendo-use-effect/src/App.js
+++ Front_end/Seção9_Context_API_e_React_Hooks/Dia1-React_Hooks-useState_e_useEffect/testes/04-entendendo-use-effect/src/App.js
@@ -1,9 +1,15 @@
 import User from './components/User'
+import './App.css'
 
 function App() {
+  const user ={
+    name: 'Roberto',
+    age: 19,
+  }
+
   return (
     <div>
-      <User name={''} age={''} />
+      <User name={ user.name } age={ user.age } />
     </div>
   );
 }
--- Front_end/Seção9_Context_API_e_React_Hooks/Dia1-React_Hooks-useState_e_useEffect/testes/04-entendendo-use-effect/src/components/User.jsx
+++ Front_end/Seção9_Context_API_e_React_Hooks/Dia1-React_Hooks-useState_e_useEffect/testes/04-entendendo-use-effect/src/components/User.jsx
@@ -1,9 +1,21 @@
 function User({name, age}) {
+  const newName = `${ name } da Silva.`
+  
+  function handleClick() {
+    // Aqui eu posso colocar um efeito colateral,
+    // como uma chamada da API
+  }
   return (
     <section>
       <h1>Dados do usuário:</h1>
-      <h2>Nome: { name }</h2>
+      <h2>Nome: { newName }</h2>
       <h2>Idade: { age }</h2>
+      <button
+      type="button"
+      onClick={ handleClick }
+      >
+        Faça um Fetch
+      </button>
     </section>
   );
 }

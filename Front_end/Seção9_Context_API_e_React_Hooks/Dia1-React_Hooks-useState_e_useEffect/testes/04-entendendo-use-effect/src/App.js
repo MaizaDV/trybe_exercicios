2f1f9d70506be019ad2289bf32f62d4ad88d411c
maiza <021maizadv@gmail.com>
2023-02-16 08:41:13 -0300

    configuração inicial do arquivo 04-entendendo-use-effect

---
+++ Front_end/Seção9_Context_API_e_React_Hooks/Dia1-React_Hooks-useState_e_useEffect/testes/04-entendendo-use-effect/src/App.js
@@ -0,0 +1,11 @@
+import User from './components/User'
+
+function App() {
+  return (
+    <div>
+      <User name={''} age={''} />
+    </div>
+  );
+}
+
+export default App;
---
+++ Front_end/Seção9_Context_API_e_React_Hooks/Dia1-React_Hooks-useState_e_useEffect/testes/04-entendendo-use-effect/src/components/User.jsx
@@ -0,0 +1,11 @@
+function User({name, age}) {
+  return (
+    <section>
+      <h1>Dados do usuário:</h1>
+      <h2>Nome: { name }</h2>
+      <h2>Idade: { age }</h2>
+    </section>
+  );
+}
+
+export default User;

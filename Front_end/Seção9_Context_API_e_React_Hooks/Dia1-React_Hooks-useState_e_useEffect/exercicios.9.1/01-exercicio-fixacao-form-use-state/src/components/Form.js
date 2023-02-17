e8c5ce7a65c5b0c533b21cc4d00c952ebd60d899
maiza <021maizadv@gmail.com>
2023-02-16 21:01:55 -0300

    configuração inicial exercício 1 de fixação

---
+++ Front_end/Seção9_Context_API_e_React_Hooks/Dia1-React_Hooks-useState_e_useEffect/exercicios.9.1/01-exercicio-fixacao-form-use-state/src/components/Form.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Form() {
+  return (
+    <form>
+      <fieldset>
+        <legend>Dados Pessoais</legend>
+      </fieldset>
+    </form>
+  );
+}
+
+export default Form;

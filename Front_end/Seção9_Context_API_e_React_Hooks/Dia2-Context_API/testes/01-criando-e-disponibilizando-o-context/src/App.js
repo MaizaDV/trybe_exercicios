45719d71c1063f9cce4163c8d3105ba4ef5e50ee
maiza <021maizadv@gmail.com>
2023-02-20 11:44:16 -0300

    injeta o context na aplicação

--- Front_end/Seção9_Context_API_e_React_Hooks/Dia2-Context_API/testes/01-criando-e-disponibilizando-o-context/src/App.js
+++ Front_end/Seção9_Context_API_e_React_Hooks/Dia2-Context_API/testes/01-criando-e-disponibilizando-o-context/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import ThemeContext from './context/ThemeContext';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Image from './components/Image';
 import './App.css';
 
 export default function App() {
-
   return (
+    <ThemeContext.Provider value={ { color: 'dark' } }>
       <div className='app light'>
         <Header />
         <Image />
         <Footer />
       </div>
+    </ThemeContext.Provider>
   );
 }

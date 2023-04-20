627f0150b32540a4f6fab25e39fe3a02ddf91330
maiza <021maizadv@gmail.com>
2023-04-20 01:51:35 -0300

    feat: Exercícios para fixar middlewares - feito

--- modulos/03-Back-end/secao4_Introducao_ao_desenvolvimento_Web_com_Node.js/Dia4-Node.js_Express_e_Middlewares/exercicios.dia.4/01-Middlewares/API_de_times_de_futebol/src/app.js
+++ modulos/03-Back-end/secao4_Introducao_ao_desenvolvimento_Web_com_Node.js/Dia4-Node.js_Express_e_Middlewares/exercicios.dia.4/01-Middlewares/API_de_times_de_futebol/src/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 
+const validateTeam = require('./middlewares/validateTeam.js');
+const existingId = require('./middlewares/existingId.js')
+
 const app = express();
 
 let nextId = 3;
@@ -10,26 +13,12 @@ const teams = [
 
 app.use(express.json());
 
-//──── middleware de validação ───────────────────────────────────────────────────────────
-const validateTeam = (req, res, next) => {
-  const requiredProperties = ['nome', 'sigla'];
-  if (requiredProperties.every((property) => property in req.body)) {
-    next(); // Chama o próximo middleware
-  } else {
-    res.sendStatus(400); // Ou já responde avisando que deu errado
-  }
-};
-
 app.get('/teams', (req, res) => res.json(teams));
 
-app.get('/teams/:id', (req, res) => {
+app.get('/teams/:id', existingId, (req, res) => {
   const id = Number(req.params.id);
   const team = teams.find(t => t.id === id);
-  if (team) {
-    res.json(team);
-  } else {
-    res.sendStatus(404);
-  }
+  return res.status(200).json(team);
 });
 
 app.post('/teams', validateTeam, (req, res) => {
@@ -39,27 +28,21 @@ app.post('/teams', validateTeam, (req, res) => {
   res.status(201).json(team);
 });
 
-app.put('/teams/:id', validateTeam, (req, res) => {
+app.put('/teams/:id', existingId, validateTeam, (req, res) => {
   const id = Number(req.params.id);
   const team = teams.find(t => t.id === id);
-  if (team) {
-    const index = teams.indexOf(team);
-    const updated = { id, ...req.body };
-    teams.splice(index, 1, updated);
-    res.status(201).json(updated);
-  } else {
-    res.sendStatus(400);
-  }
+  const index = teams.indexOf(team);
+  const updated = { id, ...req.body };
+  teams.splice(index, 1, updated);
+  return res.status(201).json(updated);
 });
 
-app.delete('/teams/:id', (req, res) => {
+app.delete('/teams/:id', existingId, (req, res) => {
   const id = Number(req.params.id);
   const team = teams.find(t => t.id === id);
-  if (team) {
-    const index = teams.indexOf(team);
-    teams.splice(index, 1);
-  }
-  res.sendStatus(204);
+  const index = teams.indexOf(team);
+  teams.splice(index, 1);
+  return res.sendStatus(204);
 });
 
 module.exports = app;
--- modulos/03-Back-end/secao4_Introducao_ao_desenvolvimento_Web_com_Node.js/Dia4-Node.js_Express_e_Middlewares/exercicios.dia.4/01-Middlewares/README.md
+++ modulos/03-Back-end/secao4_Introducao_ao_desenvolvimento_Web_com_Node.js/Dia4-Node.js_Express_e_Middlewares/exercicios.dia.4/01-Middlewares/README.md
@@ -1,9 +1,9 @@
 # Para Fixar - Middlewares
 
-🚀 1 - Crie um middleware `existingId` para garantir que o id passado como parâmetro na rota `GET /teams/:id` existe no objeto teams. Refatore essa rota para usar o middleware.
+🚀 1 - Crie um middleware `existingId` para garantir que o id passado como parâmetro na rota `GET /teams/:id` existe no objeto teams. Refatore essa rota para usar o middleware.✅
 
-🚀 2 - Reaproveite esse middleware e refatore as rotas `PUT /teams/:id` e `DELETE /teams/:id` para usarem ele também.
+🚀 2 - Reaproveite esse middleware e refatore as rotas `PUT /teams/:id` e `DELETE /teams/:id` para usarem ele também.✅
 
-🚀 3 - Mova o middleware validateTeam para o arquivo `src/middlewares/validateTeam.js`, mas continue usando o middleware nas rotas `POST /teams` e `PUT /teams/:id`.
+🚀 3 - Mova o middleware `validateTeam` para o arquivo `src/middlewares/validateTeam.js`, mas continue usando o middleware nas rotas `POST /teams` e `PUT /teams/:id`.✅
 
-🚀 4 - Mova o middleware `existingId` para o arquivo `src/middlewares/existingId.js`, mas continue usando o middleware nas rotas `PUT /teams/:id` e `DELETE /teams/:id`.
+🚀 4 - Mova o middleware `existingId` para o arquivo `src/middlewares/existingId.js`, mas continue usando o middleware nas rotas `PUT /teams/:id` e `DELETE /teams/:id`.✅

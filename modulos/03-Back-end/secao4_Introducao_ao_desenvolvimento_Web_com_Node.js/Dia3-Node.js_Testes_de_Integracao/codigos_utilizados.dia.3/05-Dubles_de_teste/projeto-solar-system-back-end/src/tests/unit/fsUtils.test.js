7940d947cad9de882473587cee47ceddcc86483f
maiza <021maizadv@gmail.com>
2023-04-29 18:40:23 -0300

    feat: solar system back-end-Dublês de teste

--- modulos/03-Back-end/secao4_Introducao_ao_desenvolvimento_Web_com_Node.js/Dia3-Node.js_Testes_de_Integracao/codigos_utilizados.dia.3/05-Dubles_de_teste/projeto-solar-system-back-end/src/tests/unit/fsUtils.test.js
+++ modulos/03-Back-end/secao4_Introducao_ao_desenvolvimento_Web_com_Node.js/Dia3-Node.js_Testes_de_Integracao/codigos_utilizados.dia.3/05-Dubles_de_teste/projeto-solar-system-back-end/src/tests/unit/fsUtils.test.js
@@ -1,10 +1,23 @@
 const { expect } = require('chai');
+const fs = require('fs');
+const sinon = require('sinon');
 
 const { readMissionsData } = require('../../utils/fsUtils');
 
+const mockMissions = JSON.stringify([
+  { id: '1', name: 'Mariner 2', year: '1962', country: 'Estados Unidos', destination: 'Vênus' },
+  { id: '2', name: 'Venera 4', year: '1967', country: 'URSS', destination: 'Vênus' },
+  { id: '3', name: 'Mariner 5', year: '1967', country: 'Estados Unidos', destination: 'Vênus' },
+]);
+
 describe('Função readMissionsData', function () {
-  it('Retorna um array', async function () {
+  it('Retorna um array com todos os elementos do arquivo json', async function () {
+    sinon.stub(fs.promises, 'readFile').resolves(mockMissions);
+
     const missions = await readMissionsData();
+    
     expect(missions).to.be.instanceOf(Array);
+    expect(missions).to.have.lengthOf(3);
+    expect(missions).not.to.have.lengthOf(5);
   });
 });

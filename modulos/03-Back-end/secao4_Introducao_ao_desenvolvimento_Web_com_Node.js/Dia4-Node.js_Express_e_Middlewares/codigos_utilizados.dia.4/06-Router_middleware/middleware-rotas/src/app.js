14a9126b596d558ee595012f9b61546b74842072
maiza <021maizadv@gmail.com>
2023-05-01 00:01:36 -0300

    [feat]-Router middleware exemplo de uma API, criando routers

--- modulos/03-Back-end/secao4_Introducao_ao_desenvolvimento_Web_com_Node.js/Dia4-Node.js_Express_e_Middlewares/codigos_utilizados.dia.4/06-Router_middleware/middleware-rotas/src/app.js
+++ modulos/03-Back-end/secao4_Introducao_ao_desenvolvimento_Web_com_Node.js/Dia4-Node.js_Express_e_Middlewares/codigos_utilizados.dia.4/06-Router_middleware/middleware-rotas/src/app.js
@@ -1,52 +1,9 @@
 const express = require('express');
 
+const routers = require('./routers');
+
 const app = express();
 
-app.get('/people', (_req, res) => {
-  return res.status(200).send('Resposta do endpoint GET /people');
-});
+app.use(routers);
 
-app.post('/people', (_req, res) => {
-  return res.status(200).send('Resposta do endpoint POST /people');
-});
-
-app.get('/people/:id', (_req, res) => {
-  return res.status(200).send('Resposta do endpoint GET /people/:id');
-});
-
-app.put('/people/:id', (_req, res) => {
-  return res.status(200).send('Resposta do endpoint PUT /people/:id');
-});
-
-app.patch('/people/:id', (_req, res) => {
-  return res.status(200).send('Resposta do endpoint PATCH /people/:id');
-});
-
-app.delete('/people/:id', (_req, res) => {
-  return res.status(200).send('Resposta do endpoint DELETE /people/:id');
-});
-
-app.get('/tasks', (_req, res) => {
-  return res.status(200).send('Resposta do endpoint GET /tasks');
-});
-
-app.post('/tasks', (_req, res) => {
-  return res.status(200).send('Resposta do endpoint POST /tasks');
-});
-
-app.get('/tasks/:id', (_req, res) => {
-  return res.status(200).send('Resposta do endpoint GET /tasks/:id');
-});
-
-app.put('/tasks/:id', (_req, res) => {
-  return res.status(200).send('Resposta do endpoint PUT /tasks/:id');
-});
-
-app.patch('/tasks/:id', (_req, res) => {
-  return res.status(200).send('Resposta do endpoint PATCH /tasks/:id');
-});
-
-app.delete('/tasks/:id', (_req, res) => {
-  return res.status(200).send('Resposta do endpoint DELETE /tasks/:id');
-});
-
+module.exports = app;

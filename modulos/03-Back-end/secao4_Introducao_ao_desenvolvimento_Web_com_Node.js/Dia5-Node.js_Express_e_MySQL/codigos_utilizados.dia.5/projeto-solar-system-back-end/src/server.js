0bc48d2cc92755b39206d236eb1280dabcd9d9ce
maiza <021maizadv@gmail.com>
2023-05-02 01:17:17 -0300

    [feat]-Configurando o MySQL no Express solar system back-end

--- modulos/03-Back-end/secao4_Introducao_ao_desenvolvimento_Web_com_Node.js/Dia5-Node.js_Express_e_MySQL/codigos_utilizados.dia.5/projeto-solar-system-back-end/src/server.js
+++ modulos/03-Back-end/secao4_Introducao_ao_desenvolvimento_Web_com_Node.js/Dia5-Node.js_Express_e_MySQL/codigos_utilizados.dia.5/projeto-solar-system-back-end/src/server.js
@@ -1,3 +1,12 @@
 const app = require('./app');
+const connection = require('./db/connection');
 
-app.listen(3005, () => console.log('Back-end rodando na porta 3005'));
+app.listen(3005, async () => {
+  console.log('Back-end rodando na porta 3005');
+  const [result] = await connection.execute('SELECT 1;');
+
+  console.log(result);
+  if (result) {
+    console.log('MySQL connection OK!');
+  }
+});

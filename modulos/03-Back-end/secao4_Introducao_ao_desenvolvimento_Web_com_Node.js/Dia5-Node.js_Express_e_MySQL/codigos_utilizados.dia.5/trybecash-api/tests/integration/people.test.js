57dbe2f8fb42b472e85a20ff71f4da16b99867f8
maiza <021maizadv@gmail.com>
2023-05-02 17:44:14 -0300

    [feat] - Trybecash API- escrevendo o primeiro teste

---
+++ modulos/03-Back-end/secao4_Introducao_ao_desenvolvimento_Web_com_Node.js/Dia5-Node.js_Express_e_MySQL/codigos_utilizados.dia.5/trybecash-api/tests/integration/people.test.js
@@ -0,0 +1,36 @@
+//  tests/integration/people.test.js
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const sinon = require('sinon');
+
+const app = require('../../src/app');
+const connection = require('../../src/db/connection');
+
+const { expect, use } = chai;
+
+use(chaiHttp);
+
+describe('Testando os endpoints de people', function () {
+  it('Testando o cadastro de uma pessoa ', async function () {
+    sinon.stub(connection, 'execute').resolves([{ insertId: 42 }]);
+
+    const response = await chai
+      .request(app)
+      .post('/people')
+      .send(
+        {
+          firstName: 'Luke',
+          lastName: 'Skywalker',
+          email: 'luke.skywalker@trybe.com',
+          phone: '851 678 4453',
+        },
+      );
+
+    expect(response.status).to.equal(201);
+    expect(response.body).to.
+      deep.equal({ message: 'Pessoa cadastrada com sucesso com o id 42' });
+  });
+
+  afterEach(sinon.restore);
+});

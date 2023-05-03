dc72cc3c87a73a0fcc101f787a970b461e818435
maiza <021maizadv@gmail.com>
2023-05-03 19:30:34 -0300

    [feat]-Trybecash API- Variáveis de Ambiente

--- modulos/03-Back-end/secao4_Introducao_ao_desenvolvimento_Web_com_Node.js/Dia5-Node.js_Express_e_MySQL/codigos_utilizados.dia.5/trybecash-api/src/db/connection.js
+++ modulos/03-Back-end/secao4_Introducao_ao_desenvolvimento_Web_com_Node.js/Dia5-Node.js_Express_e_MySQL/codigos_utilizados.dia.5/trybecash-api/src/db/connection.js
@@ -1,14 +1,14 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-  host: 'localhost',
-  port: 33060,
-  user: 'root',
-  password: 'root',
-  database: 'trybecashdb',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE_NAME,
+  waitForConnections: process.env.MYSQL_WAIT_FOR_CONNECTIONS,
+  connectionLimit: process.env.MYSQL_CONNECTION_LIMIT,
+  queueLimit: process.env.MYSQL_QUEUE_LIMIT,
 });
 
 module.exports = connection;
--- modulos/03-Back-end/secao4_Introducao_ao_desenvolvimento_Web_com_Node.js/Dia5-Node.js_Express_e_MySQL/codigos_utilizados.dia.5/trybecash-api/src/server.js
+++ modulos/03-Back-end/secao4_Introducao_ao_desenvolvimento_Web_com_Node.js/Dia5-Node.js_Express_e_MySQL/codigos_utilizados.dia.5/trybecash-api/src/server.js
@@ -5,4 +5,5 @@ const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`API TrybeCash está sendo executada na porta ${PORT}`);
+  console.log(`Valor da variável de ambiente $USER: ${process.env.USER}`);
 });

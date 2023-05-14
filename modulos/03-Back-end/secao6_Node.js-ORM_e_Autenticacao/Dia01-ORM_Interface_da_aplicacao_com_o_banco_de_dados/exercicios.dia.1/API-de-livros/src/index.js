adc1347a5d408956554c5191695b6e5c76be7d87
maiza <021maizadv@gmail.com>
2023-05-14 18:03:39 -0300

    [build]- início dos exercícios

---
+++ modulos/03-Back-end/secao6_Node.js-ORM_e_Autenticacao/Dia01-ORM_Interface_da_aplicacao_com_o_banco_de_dados/exercicios.dia.1/API-de-livros/src/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));

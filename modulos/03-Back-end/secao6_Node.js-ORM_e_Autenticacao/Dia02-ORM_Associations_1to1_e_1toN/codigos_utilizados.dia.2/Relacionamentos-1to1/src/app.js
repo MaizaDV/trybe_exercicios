5a6a04889da936d4a15b3b51f0f9c541861432e5
maiza <021maizadv@gmail.com>
2023-05-16 14:38:51 -0300

    [feat]- Relacionamentos 1:1

---
+++ modulos/03-Back-end/secao6_Node.js-ORM_e_Autenticacao/Dia02-ORM_Associations_1to1_e_1toN/codigos_utilizados.dia.2/Relacionamentos-1to1/src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const employee = require('./controllers/employee.controller');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/employees', employee.getAll);
+
+module.exports = app;
---
+++ modulos/03-Back-end/secao6_Node.js-ORM_e_Autenticacao/Dia02-ORM_Associations_1to1_e_1toN/codigos_utilizados.dia.2/Relacionamentos-1to1/src/services/employee.service.js
@@ -0,0 +1,11 @@
+const { Address, Employee } = require('../models/');
+
+const getAll = async () => {
+  const users = await Employee.findAll({
+    include: { model: Address, as: 'addresses' },
+  });
+
+  return users;
+};
+
+module.exports = { getAll };

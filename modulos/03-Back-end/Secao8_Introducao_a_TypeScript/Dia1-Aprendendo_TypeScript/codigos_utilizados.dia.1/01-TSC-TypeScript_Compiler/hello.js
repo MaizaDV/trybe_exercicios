49ea62fc82e10335bfe3576ffe046bed13a76271
maiza <021maizadv@gmail.com>
2023-05-24 15:05:10 -0300

    [feat]- TSC - TypeScript Compiler

---
+++ modulos/03-Back-end/Secao8_Introducao_a_TypeScript/Dia1-Aprendendo_TypeScript/codigos_utilizados.dia.1/01-TSC-TypeScript_Compiler/hello.js
@@ -0,0 +1,8 @@
+console.log('Hello, TypeScript!');
+
+//========================================================================================
+/*                                                                                      *
+ * Para rodar o arquivo gerado utilizando o Node, basta executar o comando no terminal. *
+ *                               comando => node hello.js                               *
+ *                                                                                      */
+//========================================================================================

8ddd4ad9e5f5214331ec85d6c8378b8eadbc1b05
maiza <021maizadv@gmail.com>
2023-08-20 06:41:12 -0300

    :sparkles:feat Estruturas condicionais

---
+++ certificacoes_eletivas/00-eletiva_java/Secao1_Fundamentos_de_Java/Dia2-Tipos_de_Dados_e_Estruturas_de_Controle_de_Fluxo/codigos_utilizados.dia.2/estruturas_condicionais_e_de_repeticao-01/src/EstruturasCondicionais.java
@@ -0,0 +1,51 @@
+import java.util.Random;
+
+/**
+ * Classe Condicional de IF/ELSE. CASO o resultado seja superior a 7, a mensagem: "PARABÉNS! VOCÊ
+ * ESTÁ NA PRÓXIMA ETAPA!". CASO O CONTRÁRIO, exibe: "POXA, NÃO FOI DESSA VEZ, MAS CONTINUE
+ * TENTANDO, ATÉ A PRÓXIMA".
+ */
+public class EstruturasCondicionais {
+
+  /**
+   * método main.
+   */
+  public static void main(String[] args) {
+    int resultado = resultadoTeste();
+    if (resultado > 7) {
+      imprimeMensagem("PARABÉNS! VOCÊ ESTÁ NA PRÓXIMA ETAPA!");
+    } else {
+      imprimeMensagem("POXA, NÃO FOI DESSA VEZ, MAS CONTINUE TENTANDO, ATÉ A PRÓXIMA");
+    }
+
+    double mediaFinal = 6.0;
+    String resultado2 = "INDEFINIDO";
+
+    if (mediaFinal >= 7) {
+      resultado2 = "APROVADA";
+    } else if (mediaFinal == 6.0) {
+      resultado2 = "REALIZAR PROVA BONUS";
+    } else {
+      resultado2 = "REPROVADA";
+    }
+    imprimeMensagem(resultado2);
+  }
+
+  /**
+   * Essa função retorna uma nota aleatória de 0 a 10.
+   */
+  static int resultadoTeste() {
+    //instância um objeto da classe Random
+    Random random = new Random();
+
+    //gera um valor aleatório qualquer do tipo int
+    int valor = random.nextInt();
+
+    //retorna o valor gerado
+    return valor;
+  }
+
+  static void imprimeMensagem(String mensagem) {
+    System.out.println(mensagem);
+  }
+}

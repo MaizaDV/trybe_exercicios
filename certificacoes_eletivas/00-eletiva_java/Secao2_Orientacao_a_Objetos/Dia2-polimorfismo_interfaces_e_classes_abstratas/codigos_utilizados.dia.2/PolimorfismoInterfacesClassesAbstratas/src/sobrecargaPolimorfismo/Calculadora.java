d60819ab2760017489c46ad22bc4d84ab18b60b1
maiza <021maizadv@gmail.com>
2023-08-26 06:06:28 -0300

    :sparkles:feat: Polimorfismo - sobrecarga

---
+++ certificacoes_eletivas/00-eletiva_java/Secao2_Orientacao_a_Objetos/Dia2-polimorfismo_interfaces_e_classes_abstratas/codigos_utilizados.dia.2/PolimorfismoInterfacesClassesAbstratas/src/sobrecargaPolimorfismo/Calculadora.java
@@ -0,0 +1,21 @@
+package sobrecargaPolimorfismo;
+
+/**
+ * Temos a classe Calculadora com três métodos com o mesmo nome, porém com assinaturas diferentes.
+ * Como podemos ver, os parâmetros estão variando em quantidade e tipo, representando o uso da
+ * técnica de Overload.
+ */
+public class Calculadora {
+
+  public int soma(int a, int b) {
+    return a + b;
+  }
+
+  public int soma(int a, int b, int c) {
+    return a + b + c;
+  }
+
+  public double soma(double a, double b) {
+    return a + b;
+  }
+}
